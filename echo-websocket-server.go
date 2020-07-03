4c42d967c5109f17bf06c539c6ee9a899c2ea179
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-03 20:51:00 +0300

    Update echo-websocket-server.go
    add Access-Control-Allow headers

--- echo-websocket-server.go
+++ echo-websocket-server.go
@@ -1,29 +1,39 @@
 package main
 
 import (
- "fmt"
- "io"
- "net/http"
- "log"
- "golang.org/x/net/websocket"
+  "fmt"
+  "io"
+  "net/http"
+  "log"
+  "golang.org/x/net/websocket"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
- fmt.Fprintf(w, "Hello World!")
+  if origin := r.Header.Get("Origin"); origin != "" {
+    w.Header().Set("Access-Control-Allow-Origin", origin)
+    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    w.Header().Set("Access-Control-Allow-Headers",
+        "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+  }
+  // Stop here if its Preflighted OPTIONS request
+  if r.Method == "OPTIONS" {
+    return
+  }
+  fmt.Fprintf(w, "Hello World!")
 }
 
 // Echo the data received on the WebSocket. 
 func EchoServer(ws *websocket.Conn) {
- io.Copy(ws, ws)
+  io.Copy(ws, ws)
 }
 
 // This example demonstrates a trivial echo server.
 func main() {
- http.Handle("/echo", websocket.Handler(EchoServer))
- // err := http.ListenAndServe(":12345", nil)
- http.HandleFunc("/",indexHandler)
- err := http.ListenAndServe(":80", nil)
- if err != nil {
-  log.Fatalln("ListenAndServe: " + err.Error())
- }
+  http.Handle("/echo", websocket.Handler(EchoServer))
+  // err := http.ListenAndServe(":12345", nil)
+  http.HandleFunc("/",indexHandler)
+  err := http.ListenAndServe(":80", nil)
+  if err != nil {
+    log.Fatalln("ListenAndServe: " + err.Error())
+  }
 }
