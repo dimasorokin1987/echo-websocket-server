8a8030a640457358629857828c488dc668c95d31
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-03 20:57:10 +0300

    Update echo-websocket-server.go
    debug: comment websocket stuff

--- echo-websocket-server.go
+++ echo-websocket-server.go
@@ -5,7 +5,7 @@ import (
   "io"
   "net/http"
   "log"
-  "golang.org/x/net/websocket"
+  //"golang.org/x/net/websocket"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
@@ -23,13 +23,13 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 }
 
 // Echo the data received on the WebSocket. 
-func EchoServer(ws *websocket.Conn) {
-  io.Copy(ws, ws)
-}
+// func EchoServer(ws *websocket.Conn) {
+//   io.Copy(ws, ws)
+// }
 
 // This example demonstrates a trivial echo server.
 func main() {
-  http.Handle("/echo", websocket.Handler(EchoServer))
+  //http.Handle("/echo", websocket.Handler(EchoServer))
   // err := http.ListenAndServe(":12345", nil)
   http.HandleFunc("/",indexHandler)
   err := http.ListenAndServe(":80", nil)
