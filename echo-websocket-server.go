be22d895891d490048e055b50f853daf5211bac6
dimasorokin1987 <58263696+dimasorokin1987@users.noreply.github.com>
2020-07-03 21:13:04 +0300

    Update echo-websocket-server.go
    decomment websocket stuff

--- echo-websocket-server.go
+++ echo-websocket-server.go
@@ -3,36 +3,35 @@ package main
 import (
   "fmt"
   "os"
-  //"io"
+  "io"
   "net/http"
   "log"
-  //"golang.org/x/net/websocket"
+  "golang.org/x/net/websocket"
 )
 
-var indexHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-  if origin := r.Header.Get("Origin"); origin != "" {
-    w.Header().Set("Access-Control-Allow-Origin", origin)
-    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    w.Header().Set("Access-Control-Allow-Headers",
-        "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-  }
-  // Stop here if its Preflighted OPTIONS request
-  if r.Method == "OPTIONS" {
-    return
-  }
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+//   if origin := r.Header.Get("Origin"); origin != "" {
+//     w.Header().Set("Access-Control-Allow-Origin", origin)
+//     w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+//     w.Header().Set("Access-Control-Allow-Headers",
+//         "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+//   }
+//   // Stop here if its Preflighted OPTIONS request
+//   if r.Method == "OPTIONS" {
+//     return
+//   }
   fmt.Fprintf(w, "Hello World!")
-})
+}
 
 // Echo the data received on the WebSocket. 
-// func EchoServer(ws *websocket.Conn) {
-//   io.Copy(ws, ws)
-// }
+func EchoServer(ws *websocket.Conn) {
+  io.Copy(ws, ws)
+}
 
-// This example demonstrates a trivial echo server.
 func main() {
-  //http.Handle("/echo", websocket.Handler(EchoServer))
+  http.Handle("/echo", websocket.Handler(EchoServer))
   // err := http.ListenAndServe(":12345", nil)
-  http.Handle("/", indexHandler)
+  http.Handle("/", http.HandlerFunc(indexHandler))
   err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
   if err != nil {
     log.Fatalln("ListenAndServe: " + err.Error())
